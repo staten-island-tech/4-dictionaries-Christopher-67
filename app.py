0f19124f7f908ad45fddf00e54573d3dcce0e10f
Christopher-67 <christophers892@nycstudents.net>
2025-10-21 10:17:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
-item = {
+
+best_buy_items = [
+{
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-}
-item = {
-    "name": "BBQ Chips\" Party Size",
-    "price": 12.49,
-    "department": "Snacks",
-    "description": "Potato chips coated with a smoky barbecue seasoning."
-}
-item = {
-    "name": "25$ apple gift card\" Prepaid Card",
+},
+{
+    "name":"25$ apple gift card\" Prepaid Card",
     "price": 25.00,
     "department": "Gift Cards",
     "description": "Prepaid card redeemable at app store and other apple retail locations"
+},
+{
+    "name": "Apple AirPods Pro 2nd Generation",
+    "price": 249.00,
+    "department": "Headphones",
+    "description": "Wireless earbuds with active noise cancellation and superior sound quality."
+},
+{
+    "name": "Dell XPS 13 Laptop",
+    "price": 999.99,
+    "department": "Computers",
+    "description": "13-inch laptop with InfinityEdge display, Intel i7 processor, and 16GB RAM."
+},
+{
+    "name": "Iphone 16 Pro Max",
+    "price": 1099.00,
+    "department": "Smartphones",
+    "description": "6.7-inch display, A15 Bionic chip, Pro camera system with LiDAR scanner."
 }
-item = {
-    "name": "Arizona iced tea\" Large can",
-    "price": 0.99,
-    "department": "Beverages",
-    "description": "23 fl oz can of Arizona Brand Iced Tea with Lemon flavor."
-}
-item = {
-    "name": "Milk\" 1 Gallon",
-    "price": 3.99,
-    "department": "Dairy",
-    "description": "1 gallon of whole milk."
-}
+]
 
-for index, item in enumerate(Walmart_items):
+for index, item in enumerate(best_buy_items):
     print(index, ":", item["name"])
-    Walmart_items = []
