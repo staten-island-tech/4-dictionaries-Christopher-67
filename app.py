269c2ccb79100366c1740ab89f777b3df32854f3
Christopher-67 <christophers892@nycstudents.net>
2025-10-30 10:17:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ best_buy_items = [
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
-    "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
+    "description": "55 inch Ultra HD Smart TV with HDR and built in streaming apps."
 },
 {
     "name":"25$ apple gift card\" Prepaid Card",
@@ -22,14 +22,50 @@ best_buy_items = [
     "name": "Dell XPS 13 Laptop",
     "price": 999.99,
     "department": "Computers",
-    "description": "13-inch laptop with InfinityEdge display, Intel i7 processor, and 16GB RAM."
+    "description": "13 inch laptop with InfinityEdge display, Intel i7 processor, and 16GB RAM."
 },
 {
     "name": "Iphone 16 Pro Max",
     "price": 1099.00,
     "department": "Smartphones",
-    "description": "6.7-inch display, A15 Bionic chip, Pro camera system with LiDAR scanner."
-}
+    "description": "6.7 inch display, A15 Bionic chip, Pro camera system with LiDAR scanner."
+},
+{
+    "name": "Playstation 5",
+    "price": 499.00,
+    "department": "Gaming",
+    "description": "Gaming console with controller, games not included."
+},
+{
+    "name": "Nintendo Switch 2",
+    "price": 399.00,
+    "department": "Gaming",
+    "description": "Handheld gaming console. Two controllers included."
+},
+{
+    "name": "Mario kart World",
+    "price": 70.00,
+    "department": "Gaming",
+    "description": "Physical copy of a game that can be played on the switch"
+},
+{
+    "name": "Roblox Gift Card",
+    "price": 100.00,
+    "department": "Gift Cards",
+    "description": "Prepaid Card redeemable on the app or website."
+},
+{
+    "name": "",
+    "price": 1099.00,
+    "department": "",
+    "description": ""
+},
+{
+    "name": "",
+    "price": 1099.00,
+    "department": "",
+    "description": ""
+},
 ]
 
 for index, item in enumerate(best_buy_items):
